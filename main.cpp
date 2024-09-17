60ad68d8f8a7af116ef2d3b0ada676a2ff2b015f
CyberCat_termux <kkrulov2006@gmail.com>
2024-09-17 12:50:17 +0300

    Add main method's  args

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include "Madi.cpp"
+#include <cstdlib>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
+  std::string path;
+  for (int i = 1; i < argc; ++i) {
+    std::string arg = argv[i];
+
+    // Проверяем аргумент '-p'
+    if (arg == "-p" && i + 1 < argc) {
+      std::string path = argv[++i]; // Следующий аргумент после '-p' будет путём
+    }
+  }
+
+  if (path.empty() && argc > 0) {
+    std::cerr << "No path specified." << std::endl;
+    return 1;
+  }
+
   Madi md = Madi();
   md.f_range();
   md.print();
